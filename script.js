c7a3e1874dae86c350b4a8fc863aaeb7444e0139
Abel Orsolya-Szekely <104093355+oszabel@users.noreply.github.com>
2024-07-05 21:36:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const url = "http://api.valyriandictionary.nhely.hu/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
-    fetch(`${url}${inpWord}`)
+    fetch(`${url}${inpWord}.json`)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
             result.innerHTML = `
             <div class="word">
-                <h3>${inpWord}</h3>
+                <h3>${data[0].word}</h3>
                 <button onclick="playSound()"><i class="fas fa-volume-up"></i></button>
             </div>
             <div class="details">
                 <p>${data[0].meanings[0].partOfSpeech}</p>
-                <p>${data[0].phonetic}</p>
+                <p>${data[0].phonetics[0].text}</p>
             </div>
             <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
+            <p class="word-etymology">${data[0].meanings[0].definitions[0].etymology}</p>
             <p class="word-example">${data[0].meanings[0].definitions[0].example || ""}</p>`;
             sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         })
